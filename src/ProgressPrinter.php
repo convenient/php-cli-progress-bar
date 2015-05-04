55c1f87dd6a73f34ee7c613c8526f65863f37582
convenient <lukerodgers90@gmail.com>
2015-05-04 21:36:55 +0100

    Refactored a bit, di

--- src/ProgressPrinter.php
+++ src/ProgressPrinter.php
@@ -5,62 +5,80 @@ class ProgressPrinter
 {
     private $totalCount = 0;
     private $currentCount = 0;
+    private $percentPerNotch = 100;
     private $countsPerNotch = false;
     private $enabled = false;
+
     /**
+     * @param int $percentPerNotch
      * @param bool $enabled
      */
-    public function __construct($enabled = false)
+    public function __construct($percentPerNotch = 4, $enabled = true)
     {
+        $this->percentPerNotch = $percentPerNotch;
         $this->enabled = $enabled;
         $this->currentCount = 0;
     }
+
     /**
+     * @param null $boolean
+     * @return bool|null
+     *
+     * @author Luke Rodgers <lukerodgers90@gmail.com>
+     */
+    public function isEnabled($boolean = null)
+    {
+        if (!is_null($boolean)) {
+            $this->enabled = ($boolean);
+        }
+        return $this->enabled;
+    }
+
+    /**
+     * Either pass in a total count value, or a traversable object which will be iterated through, counted, then reset.
+     *
      * @param $totalCount
      *
-     * @author Luke Rodgers <lr@amp.co>
+     * @author Luke Rodgers <lukerodgers90@gmail.com>
      */
     public function initProgressBar($totalCount)
     {
         if (!$this->enabled) {
             return;
         }
+
         if ($totalCount instanceof \Traversable) {
             foreach ($totalCount as $obj) {
                 $this->totalCount ++;
             }
             reset($totalCount);
+        } else {
+            $this->totalCount = intval($totalCount);
         }
-        $percentPerNotch = 4;
-        $numberOfNotches = ceil(100 / $percentPerNotch);
+
+        $numberOfNotches = ceil(100 / $this->percentPerNotch);
         $this->countsPerNotch   = ceil($this->totalCount / $numberOfNotches);
+
         for ($i = 0; $i < $numberOfNotches; $i++) {
             echo "_";
         }
         echo "\n";
     }
+
     /**
      *
-     * @author Luke Rodgers <lr@amp.co>
+     * @author Luke Rodgers <lukerodgers90@gmail.com>
      */
     public function printProgress()
     {
-        if (!$this->enabled) {
+        if (!$this->enabled && $this->countsPerNotch) {
             return;
         }
         if (($this->currentCount++ % $this->countsPerNotch == 0)) {
             echo "%";
         }
-    }
-    /**
-     *
-     * @author Luke Rodgers <lr@amp.co>
-     */
-    public function closeProgressBar()
-    {
-        if ($this->enabled) {
-            return;
+        if ($this->currentCount == $this->totalCount) {
+            echo "\n";
         }
-        echo "\n";
     }
 }
